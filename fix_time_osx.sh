075f7b887bf6bb9dd5107dad9ff454e0561fa7cc
LefonApp <46104566+LefonApp@users.noreply.github.com>
2021-08-19 16:40:57 +0800

    Update fix_time_osx.sh

--- fix_time_osx.sh
+++ fix_time_osx.sh
@@ -1,32 +1,20 @@
 #!/bin/sh
 #Script auto fix time on hackintosh
 #Version 1.1: Otc 11, 2015
+#Version 1.2: Jan 17, 2021 by milaoshu1020
 #OSX: 10.6 and above
 
 echo "This script required to run as root"
 
-sudo curl -o "/tmp/localtime-toggle" "https://raw.githubusercontent.com/hieplpvip/LocalTime-Toggle/master/sbin/localtime-toggle"
-sudo curl -o "/tmp/org.osx86.localtime-toggle.plist" "https://raw.githubusercontent.com/hieplpvip/LocalTime-Toggle/master/Library/LaunchDaemons/org.osx86.localtime-toggle.plist"
-
-echo "Downloading required file"
-
-# exit if error
-if [ "$?" != "0" ]; then
-echo "Error: Download failure, verify network"
-exit 1
-fi
-
 echo "Copy file to destination place..."
 
-sudo cp -R "/tmp/localtime-toggle" "/sbin/"
-sudo cp -R "/tmp/org.osx86.localtime-toggle.plist" "/Library/LaunchDaemons/"
-
-sudo rm /tmp/localtime-toggle
-sudo rm /tmp/org.osx86.localtime-toggle.plist
+mkdir /Users/Shared/sbin
+sudo cp -R ./sbin/localtime-toggle /Users/Shared/sbin/
+sudo cp -R ./Library/LaunchDaemons/org.osx86.localtime-toggle.plist /Library/LaunchDaemons/
 
 echo "Chmod localtime-toggle..."
 
-sudo chmod +x /sbin/localtime-toggle
+sudo chmod +x /Users/Shared/sbin/localtime-toggle
 
 echo "Chmod org.osx86.localtime-toggle.plist..."
 
